e41619717532e27f4978fe1690296135e85647ba
Juan Zwarts <48238264+FROST113@users.noreply.github.com>
2023-06-04 08:29:16 +0200

    DOM-Manipulation

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,23 +1,11 @@
-// save to saved items page
-function saveForLater(articleId) {
-    // Get the HTML block to save
-    const articleContainer = document.querySelector(`#article-${articleId}`).outerHTML;
-  
-    // Save the article container to localStorage
-    localStorage.setItem(`saved-article-${articleId}`, articleContainer);
-  
-    // Alert the user that the item was saved and how many items there are
-    const savedItemsCount = Object.keys(localStorage).filter(key => key.startsWith('saved-article-')).length;
-    alert(`Item saved! You have ${savedItemsCount} saved item(s).`);
-  }
-  
-  
 // Get the saved items container on the saved items page
 const savedItemsContainer = document.querySelector('#saved-items-container');
 
+// Create a document fragment to hold the saved articles
+const fragment = document.createDocumentFragment();
+
 // Loop through all keys in localStorage that start with 'saved-article-'
-for (let i = 0; i < localStorage.length; i++) {
-  const key = localStorage.key(i);
+for (const key in localStorage) {
   if (key.startsWith('saved-article-')) {
     // Create a new div element to hold the saved article container
     const savedArticleContainer = document.createElement('div');
@@ -27,9 +15,26 @@ for (let i = 0; i < localStorage.length; i++) {
     // Set the HTML of the new div to the saved article container HTML
     savedArticleContainer.innerHTML = localStorage.getItem(key);
 
-    // Add the saved article container to the saved items container
-    savedItemsContainer.appendChild(savedArticleContainer);
+    // Append the saved article container to the fragment
+    fragment.appendChild(savedArticleContainer);
   }
+}
+
+// Append the fragment to the saved items container
+savedItemsContainer.appendChild(fragment);
+
+
+// save to saved items page
+function saveForLater(articleId) {
+  // Get the HTML block to save
+  const articleContainer = document.querySelector(`#article-${articleId}`).outerHTML;
+
+  // Save the article container to localStorage
+  localStorage.setItem(`saved-article-${articleId}`, articleContainer);
+
+  // Alert the user that the item was saved and how many items there are
+  const savedItemsCount = Object.keys(localStorage).filter(key => key.startsWith('saved-article-')).length;
+  alert(`Item saved! You have ${savedItemsCount} saved item(s).`);
 }
 
 
@@ -68,4 +73,3 @@ function addLike(articleId) {
   likeCount++;
   document.getElementById('likeCount' + articleId).textContent = likeCount;
 }
-
